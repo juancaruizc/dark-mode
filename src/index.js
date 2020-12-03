88174c4d8230da2574a9d409ff2f7dc0f16b11ed
juancaruizc <juancruizcruz@gmail.com>
2020-12-02 18:48:35 -0700

    MVP met

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
+import {useDarkMode} from './hooks/useDarkMode'
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
@@ -9,7 +9,7 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode();
 
   useEffect(() => {
     axios
@@ -29,7 +29,3 @@ const App = () => {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
-//test
-//test
-//test
